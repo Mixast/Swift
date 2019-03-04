5c7722ff5e92eea05743d6ef57d2544b2e1bbb34
LexLuthor <mixast@.mail.ru>
2019-03-05 01:15:32 +0300

    Сделал составление базы более коректным. Ранее было сделано, проверки на пустые поля, и на правильность введенных данных. При правильном введении данных осушествляется переход на приветственное поле. C которого можно вернуться назад или прейти к основному функцианалу ( по свайпам влево или вправо)

--- 1l_2S/1l_2S_Mihailov_Mihail/LoginViewController.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/LoginViewController.swift
@@ -9,39 +9,54 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+// MARK: - Connect IBOutlet
     @IBOutlet weak var substrate: UILabel!
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var button: UIButton!
-    
+// MARK: - Connect IBAction
     @IBAction func buttonAction(_ sender: Any) {
-        var flack = false
-        for i in 1...base.count {    // поиск по струтуре
-            if base[i-1].login == loginTextField.text {
-                if base[i-1].password == passwordTextField.text {
-                        let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
-                        detailVC.transportLine = base[i-1].id
-                        self.navigationController?.pushViewController(detailVC, animated: true)
-                     flack = true
+        // Запускаем процесс составления базы (это кастыль решил так реализовать). И когда база составлена ищу в ней связку login/password
+        fillingBase {
+            DispatchQueue.main.async {
+                var flack = false
+                for i in 1...base.count {
+                    
+                    guard  let login = self.loginTextField.text, login.count > 0 else {
+                        self.showAlert(massage: "Поле Login пустое", title: "Error")
+                        return
+                    }
+                    
+                    guard let password = self.passwordTextField.text, password.count > 0   else {
+                        self.showAlert(massage: "Поле Password пустое", title: "Error")
+                        return
+                    }
+                    
+                    if base[i-1].login == self.loginTextField.text {
+                        if base[i-1].password == self.passwordTextField.text {
+                            transportLine = base[i-1].id
+                            self.performSegue(withIdentifier: "goToInfo", sender: self)
+                            flack = true
+                        }
+                    }
+                }
+                
+                if flack == false {
+                    self.showAlert(massage: "Пользователь в базе не зарегистрирован", title: "Error")
                 }
             }
         }
-        if flack == false {
-        showAlert(massage: "Вы ввели неправильные данные", title: "Error")
-        }
     }
     
-    private func designFor(label: UILabel) {  // Кастомизирую подложку
-//        label.layer.masksToBounds = true
-//        label.layer.cornerRadius = 12
+// MARK: - Функции кастомизации UILabel и UIButton
+    private func designFor(label: UILabel) {
         label.backgroundColor = UIColor(patternImage: UIImage(named: "ww33.jpg")!)
         label.layer.shadowOffset = CGSize(width: 10, height: 10)
         label.layer.shadowColor = UIColor.black.cgColor
         label.layer.shadowOpacity = 0.6
     }
     
-    private func designFor(button: UIButton) {  // Кастамизирую кнопку
+    private func designFor(button: UIButton) {
         button.layer.borderWidth = 1
         button.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         button.layer.cornerRadius = 10
@@ -51,26 +66,23 @@ class LoginViewController: UIViewController {
         button.layer.shadowOpacity = 0.6
     }
     
+// MARK: - Main body
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "Login Field"
-        designFor(label: substrate)
-        designFor(button: button)
+        navigationItem.title = "Login Field"  // Имя поля
+        designFor(label: substrate)  // Кастомизация подложки substrate
+        designFor(button: button)    // Костомизация кнопри Connect
         
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true
-        
-        fillingBase {
-            DispatchQueue.main.async {
-            }
-        }
+        self.navigationController?.navigationBar.isTranslucent = true // Делаем прозрачным navigationBar
         
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        self.view.endEditing(true) // Убираем клавиатуру после ввода
     }
     
     func showAlert (massage: String, title: String) {    // Вывод ошибки если пользователь ввел неправильно данные
