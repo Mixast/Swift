c167e23c2e22f21d81bea9b2d0c2f28641bf87e7
LexLuthor <mixast@.mail.ru>
2019-04-05 19:06:52 +0300

    Убран хлам

--- 1l_2S/1l_2S_Mihailov_Mihail/History Controller/TableViewHistoryHeader.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/History Controller/TableViewHistoryHeader.swift
@@ -8,12 +8,13 @@ class TableViewHistoryHeader: UITableViewHeaderFooterView {
     @IBOutlet weak var likeButton: LikeButton!
     @IBOutlet weak var likeLabel: UILabel!
     
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+
+ 
     override func draw(_ rect: CGRect) {
-        // Drawing code
+   
     }
-    */
+ 
+    
+    
 
 }
