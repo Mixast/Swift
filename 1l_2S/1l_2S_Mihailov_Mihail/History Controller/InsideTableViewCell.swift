7fab8e31395003e52403723080d8f270e2587501
LexLuthor <mixast@.mail.ru>
2019-04-05 19:03:05 +0300

    Убрат хлам

--- 1l_2S/1l_2S_Mihailov_Mihail/History Controller/InsideTableViewCell.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/History Controller/InsideTableViewCell.swift
@@ -1,11 +1,3 @@
-//
-//  InsideTableViewCell.swift
-//  1l_2S_Mihailov_Mihail
-//
-//  Created by Лекс Лютер on 12/03/2019.
-//  Copyright © 2019 Лекс Лютер. All rights reserved.
-//
-
 import UIKit
 
 class InsideTableViewCell: UITableViewCell {
