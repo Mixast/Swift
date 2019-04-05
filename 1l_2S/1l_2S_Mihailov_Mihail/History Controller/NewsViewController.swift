9f26e9d187452049254d64219cc6accb62d85235
LexLuthor <mixast@.mail.ru>
2019-04-05 19:06:20 +0300

    Дабавлено увеличение и уменшение картинки новости по нажатию

--- 1l_2S/1l_2S_Mihailov_Mihail/History Controller/NewsViewController.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/History Controller/NewsViewController.swift
@@ -1,14 +1,19 @@
 import UIKit
 
 class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource {
-    var statusLike: (Int , Bool) = (0, true)
+
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var dayControl: SelectDayControl!
-    
+    var rowSizer = [Razmermer]()
+    var flack = true
     
     override func viewDidLoad() {
+
         fillingLikeBase { // Составляем список новостей
             DispatchQueue.main.async {
+                for i in 0...likeBase.count {
+                    self.rowSizer.append(Razmermer(id: i, size: 250))
+                }
                 self.tableView.reloadData()
             }
         }
@@ -20,6 +25,7 @@ class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDat
 
         tableView.backgroundColor = .clear
         tableView.tableFooterView = UIView() //Убираем пустые строки
+
         
         // Кастомный Header
 
@@ -34,9 +40,9 @@ class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDat
 
     // MARK: - Создание секций и их тайтлов
         func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-
+            
         let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "TableViewHistoryHeader") as? TableViewHistoryHeader
-
+        header?.tag = section + 22
         header?.historyView.backgroundColor = #colorLiteral(red: 0.1247105226, green: 0.1294333935, blue: 0.1380615532, alpha: 1)
         header?.historyButton.setTitle(likeBase[section].name, for: .normal)
         header?.historyButton.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.left
@@ -52,10 +58,13 @@ class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDat
 
         header?.likeButton.addTarget(self, action: #selector(handleExpandLike), for: .touchDown)
         header?.likeButton.tag = section
-        if statusLike.0 == header?.likeButton.tag {
-            header?.likeButton.bool = statusLike.1
-        }
 
+        if likeBase[section].likeStatus == true {
+            header?.likeButton.bool = true
+        } else {
+            header?.likeButton.bool = false
+        }
+            
         return header
     }
     
@@ -70,16 +79,12 @@ class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDat
     }
 
     @objc func handleExpandLike(button: LikeButton) {
-        if button.bool {
-            statusLike = (button.tag, false)
-
-        } else {
-            statusLike = (button.tag, true)
-        }
-        if button.bool {
+        if likeBase[button.tag].likeStatus == true {
             likeBase[button.tag].likeMetr+=1
+            likeBase[button.tag].likeStatus = false
         } else {
             likeBase[button.tag].likeMetr-=1
+            likeBase[button.tag].likeStatus = true
         }
         tableView.reloadData()
     }
@@ -91,7 +96,7 @@ class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 60
+        return 50
     }
     
     // MARK: - numberOfRowsInSection
@@ -104,21 +109,87 @@ class NewsViewController: UIViewController, UITableViewDelegate,  UITableViewDat
         }
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(self.rowSizer[indexPath.section].size)
+        
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let image = UIImage(named: likeBase[indexPath.section].image + ".jpg")
-        let imageTitle = image?.scaled(to: 225)
+        let imageTitle = image?.scaled(to: CGFloat(self.rowSizer[indexPath.section].size - 25))
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "InsideTableViewCell") as? InsideTableViewCell else { return UITableViewCell() }
         cell.textLabel?.text = likeBase[indexPath.section].news
         cell.textLabel?.lineBreakMode = .byWordWrapping
         cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.tag = (indexPath.section+3)*3
+        
         cell.imageView?.image = imageTitle                                           // Добавляем аватарку
         cell.imageView?.layer.cornerRadius = 30                                     // Делаем её круглой
         cell.imageView?.layer.masksToBounds = true
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        cell.imageView?.isUserInteractionEnabled = true
+        cell.imageView?.addGestureRecognizer(tapGestureRecognizer)
+        cell.imageView?.tag = (indexPath.section+2)*2
         
         cell.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        
+        
         return cell
     }
  
-
+    // Анимация по нажатию на картинку
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+     
+        
+        let touch = tapGestureRecognizer.location(in: tableView)
+        if let indexPath = tableView.indexPathForRow(at: touch) {
+            let originX = self.view.viewWithTag((indexPath.section+2)*2)!.frame.origin.x
+            let originY = self.view.viewWithTag((indexPath.section+2)*2)!.frame.origin.y
+            let sizeWidth = self.view.viewWithTag((indexPath.section+2)*2)!.frame.size.width
+            let sizeHeight = self.view.viewWithTag((indexPath.section+2)*2)!.frame.size.height
+        
+            let IMGoriginX = self.view.viewWithTag((indexPath.section+3)*3)!.frame.origin.x
+            let IMGoriginY = self.view.viewWithTag((indexPath.section+3)*3)!.frame.origin.y
+            let IMGsizeWidth = self.view.viewWithTag((indexPath.section+3)*3)!.frame.size.width
+            let IMGsizeHeight = self.view.viewWithTag((indexPath.section+3)*3)!.frame.size.height
+ 
+            if self.rowSizer[indexPath.section].flack {
+                
+                
+                self.rowSizer[indexPath.section].flack = false
+                UIView.animate(withDuration: 2,
+                           delay: 0.2,
+                           usingSpringWithDamping: 0.4,
+                           initialSpringVelocity: 4,
+                           options: [.curveEaseOut , .beginFromCurrentState],
+                           animations: {
+                            self.rowSizer[indexPath.section].size =  350
+                            self.tableView.beginUpdates()
+                            self.tableView.endUpdates()
+                            self.view.viewWithTag((indexPath.section+2)*2)!.frame = CGRect(x: originX, y: originY, width: sizeWidth + 100, height: sizeHeight + 100)
+                            self.view.viewWithTag((indexPath.section+3)*3)!.frame = CGRect(x: IMGoriginX + 100, y: IMGoriginY, width: IMGsizeWidth - 100, height: IMGsizeHeight + 100)
+                })
+            } else {
+                
+                self.rowSizer[indexPath.section].flack = true
+                UIView.animate(withDuration: 2,
+                               delay: 0.2,
+                               usingSpringWithDamping: 0.4,
+                               initialSpringVelocity: 4,
+                               options: [],
+                               animations: {
+                                self.view.viewWithTag((indexPath.section+2)*2)!.frame = CGRect(x: originX, y: originY, width: sizeWidth - 100, height: sizeHeight - 100)
+                                self.view.viewWithTag((indexPath.section+3)*3)!.frame = CGRect(x: IMGoriginX - 100, y: IMGoriginY, width: IMGsizeWidth + 100, height: IMGsizeHeight - 100)
+                                self.rowSizer[indexPath.section].size =  250
+                                self.tableView.beginUpdates()
+                                self.tableView.endUpdates()
+                                
+                })
+            }
+        }
+    }
+    
 }
+
