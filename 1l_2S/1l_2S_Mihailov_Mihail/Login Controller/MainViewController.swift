481144d203e7d0dd7c77a684c384300da79743ca
LexLuthor <mixast@.mail.ru>
2019-04-10 17:06:22 +0300

    Добавлена картинка машинки и анимация её передвижения. При нахождении машинки в правой краю View  осуществляется переход на другой view

--- 1l_2S/1l_2S_Mihailov_Mihail/Login Controller/MainViewController.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/Login Controller/MainViewController.swift
@@ -4,8 +4,12 @@ import SafariServices
 
 class MainViewController: UIViewController {
     // MARK: - IBOutlet и IBAction
+    var animator: UIViewPropertyAnimator?
+    
+    
     
     @IBOutlet weak var messgeField: UITextView!
+    @IBOutlet weak var carImage: UIImageView!
     
     @IBAction func watchigButton(_ sender: UIButton) { // Переход на страницу поиска в интернете
         
@@ -25,19 +29,18 @@ class MainViewController: UIViewController {
         navigationItem.title = "Main Board"
         let emptyView = UIView(frame: .zero)    // Делаем navigationItem прозрачным
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: emptyView)
-        
+
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
         self.view.addGestureRecognizer(swipeRight)  //Ловим свайп
         
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGestureTwo))
-        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-        self.view.addGestureRecognizer(swipeLeft) //Ловим свайп
+        let swipe = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        self.view.addGestureRecognizer(swipe) //Ловим свайп
         
-        messgeField.text = "Приветствую " + base[transportLine].name + ", давненько мы не виделись." + "\n" + "Ваше любимое анимэ " + base[transportLine].favoriteАnime[0].name + " ждет))" + "\n" + "По свайпу вправа вы вернетесь на поле логина. <<-" + "\n" + "По свайпу влево вы перейдете дальше. Удачи) ->>"
+        messgeField.text = "Приветствую " + base[transportLine].name + ", давненько мы не виделись." + "\n" + "Ваше любимое анимэ " + base[transportLine].favoriteАnime[0].name + " ждет))" + "\n" + "По свайпу вправа вы вернетесь на поле логина. <<-" + "\n" + "Для продолжения доведите машинку до правога края. Удачи) ->>"
         
     }
-    // MARK: - Переход по свайпу вправо
+//     MARK: - Переход по свайпу вправо
     @objc func respondToSwipeGesture(gesture: UIGestureRecognizer)  // Возврат к предыдушему меню по свайпу
     {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer
@@ -51,22 +54,26 @@ class MainViewController: UIViewController {
             }
         }
     }
-    // MARK: - Переход по свайпу влево
     
-    @objc func respondToSwipeGestureTwo(gesture: UIGestureRecognizer)  // Возврат к предыдушему меню по свайпу
-    {
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer
-        {
-            switch swipeGesture.direction
-            {
-            case UISwipeGestureRecognizer.Direction.left:
+//     MARK: - Анимация передвижения машинки
+    @objc func pan(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            animator = UIViewPropertyAnimator(duration: 6, curve: .linear, animations: {
+                self.carImage.frame.origin.x += self.view.frame.maxX - (self.carImage.frame.origin.x + self.carImage.frame.size.width + 50)
+            })
+            animator?.pauseAnimation()
+        case .changed:
+            let translation = recognizer.translation(in: self.view)
+            animator?.fractionComplete = translation.x / 400
+        case .ended:
+            animator?.stopAnimation(true)
+            if self.carImage.frame.origin.x >= self.view.frame.maxX - (self.carImage.frame.size.width + 150) {
                 self.performSegue(withIdentifier: "goToStart", sender: self)
-            default:
-                break
             }
+    
+        default: return
         }
     }
-
-
-
+   
 }
