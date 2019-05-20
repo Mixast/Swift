d79d8e5c74941f3f54c59cb1dd4cccce73509899
LexLuthor <mixast@.mail.ru>
2019-05-20 19:32:06 +0300

    Пытаюсь загружать картинки по мере просмотра ( пока получается криво, c тормозами и нормального решения не нашел )

--- 1l_2S/1l_2S_Mihailov_Mihail/Anime Controller/Anime Deck/TabOneCollectionViewController.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/Anime Controller/Anime Deck/TabOneCollectionViewController.swift
@@ -2,7 +2,8 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class TabOneCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class TabOneCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDataSourcePrefetching, UICollectionViewDelegateFlowLayout {
+    
     @IBOutlet weak var backgoundImage: UIImageView!
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -12,21 +13,35 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
     
     let mainProfile = MainProfile.instance
     let friendProfile = FriendProfile.instance
+    var tasks = [URLSessionDataTask?](repeating: nil, count: 100)
     
+    var count = 0
     var transportLine = Int()
-    var profile = String()
-
+    var profile: String = "" {
+        didSet {
+            if profile == "mainProfile" {
+                count = mainProfile.favoriteАnime[transportLine].colectionImage.count
+            } else {
+                count = friendProfile.favoriteАnime[transportLine].colectionImage.count
+            }
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.prefetchDataSource = self
         
+//        getImage(infoProfile: profile, transportLine: transportLine)
         if profile == "mainProfile" {
             navigationItem.title = mainProfile.favoriteАnime[transportLine].name  // Имя поля
             self.backgoundImage.image = mainProfile.favoriteАnime[transportLine].avatarImage
-        } else {            
+        } else {
             self.setNavigationBar()
             self.backgoundImage.image = UIImage(named: "Hp.jpg")
-            let imageURL = NSURL(string: "https://shikimori.org" + friendProfile.favoriteАnime[transportLine].colectionImage[0])
+            let imageURL = NSURL(string: "https://shikimori.one" + friendProfile.favoriteАnime[transportLine].colectionImage[0])
             let queue = DispatchQueue.global(qos: .utility)
             queue.async{
                 if let data = try? Data(contentsOf: imageURL! as URL){
@@ -36,6 +51,7 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
                     }
                 }
             }
+            
             let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
             swipeRight.direction = UISwipeGestureRecognizer.Direction.right
             self.view.addGestureRecognizer(swipeRight)  //Ловим свайп
@@ -49,68 +65,87 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
         
         sizeWidthCell = self.view.frame.size.width / 3
         sizeHeightCell = self.view.frame.size.height / 4
-    
-        //     MARK: - Очищаем память
-        
-        DispatchQueue.main.async {
-            for i in stride(from: 1, through: self.mainProfile.favoriteАnime.count, by: 1) {
-                if self.mainProfile.favoriteАnime[i-1].close {
-                    self.mainProfile.favoriteАnime[i-1].close = false
-                    self.mainProfile.favoriteАnime[i-1].colectionImG.removeAll()
-                }
-            }
-            
-            for i in stride(from: 1, through: self.friendProfile.favoriteАnime.count, by: 1) {
-                if self.friendProfile.favoriteАnime[i-1].close {
-                    self.friendProfile.favoriteАnime[i-1].close = false
-                    self.friendProfile.favoriteАnime[i-1].colectionImG.removeAll()
-                }
-            }
-        }
-        
-        
-//     MARK: - Параллельно прогружаем скриншоты
-        if profile == "mainProfile" {
-            if mainProfile.favoriteАnime[transportLine].colectionImage.count != 0 {
-                for i in 1...mainProfile.favoriteАnime[transportLine].colectionImage.count {
-                    if self.mainProfile.favoriteАnime[self.transportLine].close { } else {
-                        let imageURL = NSURL(string: "https://shikimori.org" + mainProfile.favoriteАnime[transportLine].colectionImage[i-1])
-                        let queue = DispatchQueue.global(qos: .utility)
-                        queue.async{
-                            if let data = try? Data(contentsOf: imageURL! as URL){
-                                DispatchQueue.main.async {
-                                    self.mainProfile.favoriteАnime[self.transportLine].colectionImG[i-1] = UIImage(data: data)!
-                                    let indexPath = IndexPath(row: i-1, section: 0)
-                                    self.collectionView.reloadItems(at: [indexPath])
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            if friendProfile.favoriteАnime[transportLine].colectionImage.count != 0 {
-                for i in 1...friendProfile.favoriteАnime[transportLine].colectionImage.count {
-                    if self.mainProfile.favoriteАnime[self.transportLine].close { } else {
-                        let imageURL = NSURL(string: "https://shikimori.org" + friendProfile.favoriteАnime[transportLine].colectionImage[i-1])
-                        let queue = DispatchQueue.global(qos: .utility)
-                        queue.async{
-                            if let data = try? Data(contentsOf: imageURL! as URL){
-                                DispatchQueue.main.async {
-                                    self.friendProfile.favoriteАnime[self.transportLine].colectionImG[i-1] = UIImage(data: data)!
-                                    let indexPath = IndexPath(row: i-1, section: 0)
-                                    self.collectionView.reloadItems(at: [indexPath])
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
         
     }
     
-    func setNavigationBar() {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        clearMemory()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        if self.flack == true {
+            self.sizeWidthCell = self.view.frame.size.width / 3
+            self.sizeHeightCell = self.view.frame.size.height / 4
+        } else {
+            self.sizeWidthCell = self.view.frame.size.width
+            self.sizeHeightCell = self.view.frame.size.height
+        }
+        
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    
+    
+    
+//    //     MARK: - Получение картинок
+//    private func getImage(infoProfile: String, transportLine: Int) {
+//        if infoProfile == "mainProfile" {
+//            if mainProfile.favoriteАnime[transportLine].colectionImage.count != 0 {
+//                print(self._IQDescription())
+//                for i in 1...mainProfile.favoriteАnime[transportLine].colectionImage.count {
+//
+//                    if mainProfile.favoriteАnime[transportLine].close { } else {
+//                        let imageURL = NSURL(string: "https://shikimori.one" + mainProfile.favoriteАnime[transportLine].colectionImage[i-1])
+//                        let queue = DispatchQueue.global(qos: .utility)
+//                        queue.async{
+//                            if let data = try? Data(contentsOf: imageURL! as URL) {
+//                                DispatchQueue.main.async { [weak self] in
+//                                    guard let self = self else { return }
+//
+//                                    self.mainProfile.favoriteАnime[transportLine].colectionImG[i-1] = UIImage(data: data)!
+//
+//                                    let indexPath = IndexPath(row: i-1, section: 0)
+//                                    self.collectionView.reloadItems(at: [indexPath])
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                mainProfile.favoriteАnime[transportLine].close = true
+//            }
+//        } else {
+//            if friendProfile.favoriteАnime[transportLine].colectionImage.count != 0 {
+//                for i in 1...friendProfile.favoriteАnime[transportLine].colectionImage.count {
+//                    if self.mainProfile.favoriteАnime[self.transportLine].close { } else {
+//                        let imageURL = NSURL(string: "https://shikimori.one" + friendProfile.favoriteАnime[transportLine].colectionImage[i-1])
+//                        let queue = DispatchQueue.global(qos: .utility)
+//                        queue.async{
+//                            if let data = try? Data(contentsOf: imageURL! as URL){
+//                                DispatchQueue.main.async {
+//                                    self.friendProfile.favoriteАnime[self.transportLine].colectionImG[i-1] = UIImage(data: data)!
+//                                    let indexPath = IndexPath(row: i-1, section: 0)
+//                                    self.collectionView.reloadItems(at: [indexPath])
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//                friendProfile.favoriteАnime[transportLine].close = true
+//            }
+//        }
+//    }
+
+    
+    
+    
+    private func setNavigationBar() {
         let navBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 50))
         navBar.backgroundColor = .clear
         navBar.setBackgroundImage(UIImage(), for: .default)
@@ -138,7 +173,6 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
             switch swipeGesture.direction
             {
             case UISwipeGestureRecognizer.Direction.right:
-                mainProfile.favoriteАnime[transportLine].close = true
                 dismiss(animated: true)
             default:
                 break
@@ -147,18 +181,17 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
     }
     
     @objc func handleShowIndexPath() {      //Подключение любой кнопки
-        friendProfile.favoriteАnime[transportLine].close = true
         dismiss(animated: true)
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if profile == "mainProfile" {
-            return mainProfile.favoriteАnime[transportLine].colectionImage.count
+            return mainProfile.favoriteАnime[transportLine].colectionImG.count
         } else {
            return friendProfile.favoriteАnime[transportLine].colectionImage.count
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -166,36 +199,109 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
-  
-        if profile == "mainProfile" {
-            cell.imageAnime.image = mainProfile.favoriteАnime[transportLine].colectionImG[indexPath.row]
-        } else {
-            cell.imageAnime.image = friendProfile.favoriteАnime[transportLine].colectionImG[indexPath.row]
-        }
-        cell.imageAnime.translatesAutoresizingMaskIntoConstraints = false
         
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
+
+        if mainProfile.favoriteАnime[transportLine].colectionImG[indexPath.row].pngData() != nil {
+            cell.imageAnime.image = mainProfile.favoriteАnime[transportLine].colectionImG[indexPath.row]
+        }
+        else {
+            cell.imageAnime.image = #imageLiteral(resourceName: "icons8-vnim-50")
+            if (0...8).contains(indexPath.row) {
+                DispatchQueue.global().async {
+                    self.mainProfile.favoriteАnime[self.transportLine].close = true
+                    self.requestImage(forIndex: indexPath)
+                }
+            }
+
+        }
+        
+        
+//        if profile == "mainProfile" {
+//            cell.imageAnime.image = mainProfile.favoriteАnime[transportLine].colectionImG[indexPath.row]
+//        } else {
+//            cell.imageAnime.image = friendProfile.favoriteАnime[transportLine].colectionImG[indexPath.row]
+//        }
+//
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-        cell.imageAnime.isUserInteractionEnabled = true
         cell.imageAnime.addGestureRecognizer(tapGestureRecognizer)
         cell.imageAnime.tag = ((indexPath.section*100)+indexPath.row+1)
         return cell
     }
     
-// MARK: -  Анимация увеличения картинки
     
+    // Пытаюсь прогружать картинки при пролистывании - пока не знаю как убрать тормоза при пролистывании ((
+    
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        
+        for indexPath in indexPaths {
+            
+            if mainProfile.favoriteАnime[transportLine].colectionImG[indexPath.row].pngData() == nil {
+                 DispatchQueue.global().async {
+                    self.requestImage(forIndex: indexPath)
+                }
+            }
+        }
+    
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        
+        for indexPath in indexPaths {
+            
+            if let task = tasks[indexPath.row] {
+                if task.state != URLSessionTask.State.canceling {
+                    task.cancel()
+                }
+            }
+        }
+    }
+
+    func requestImage(forIndex: IndexPath) {
+        
+        var task: URLSessionDataTask
+        
+        if tasks[forIndex.row] != nil
+            && tasks[forIndex.row]!.state == URLSessionTask.State.running {
+            // Wait for task to finish
+            return
+        }
+        
+        task = getTask(forIndex: forIndex)
+        tasks[forIndex.row] = task
+        task.resume()
+    }
+    
+    func getTask(forIndex: IndexPath) -> URLSessionDataTask  {
+        let imgURL = URL(string: "https://shikimori.one" + mainProfile.favoriteАnime[transportLine].colectionImage[forIndex.row])!
+        
+        return URLSession.shared.dataTask(with: imgURL) { data, response, error in
+            DispatchQueue.global().async {
+                guard let data = data, error == nil else { return }
+                DispatchQueue.main.async() {
+                    let image = UIImage(data: data)!
+                    self.mainProfile.favoriteАnime[self.transportLine].colectionImG[forIndex.row] = image
+                    self.collectionView.reloadItems(at: [forIndex])
+                }
+            }
+        }
+    }
+    
+    // ^^^
+    
+// MARK: -  Анимация увеличения картинки
+
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
     {
-        
+
         let touch = tapGestureRecognizer.location(in: collectionView)
         if let indexPath = collectionView.indexPathForItem(at: touch) {
             let originX = self.view.viewWithTag((indexPath.section*100)+indexPath.row+1)!.frame.origin.x
             let originY = self.view.viewWithTag((indexPath.section*100)+indexPath.row+1)!.frame.origin.y
             let originSizeWidth = self.view.viewWithTag((indexPath.section*100)+indexPath.row+1)!.frame.size.width
             let originSizeHeight = self.view.viewWithTag((indexPath.section*100)+indexPath.row+1)!.frame.size.height
-            
+
             UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
                     self.view.viewWithTag((indexPath.section*100)+indexPath.row+1)!.frame = CGRect(x: originX, y: originY, width: originSizeWidth+80, height: originSizeHeight+80)
@@ -223,20 +329,6 @@ class TabOneCollectionViewController: UIViewController, UICollectionViewDataSour
                 })
             }
         }
-    }
-    
-    
-    override func viewDidLayoutSubviews() {
-        
-        if self.flack == true {
-            self.sizeWidthCell = self.view.frame.size.width / 3
-            self.sizeHeightCell = self.view.frame.size.height / 4
-        } else {
-            self.sizeWidthCell = self.view.frame.size.width
-            self.sizeHeightCell = self.view.frame.size.height
-        }
-        
-        collectionView.collectionViewLayout.invalidateLayout()
     }
     
 }
