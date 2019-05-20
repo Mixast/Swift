398b26a21a85d5effbb15a19fa4ead3aed2b704f
LexLuthor <mixast@.mail.ru>
2019-05-20 19:32:32 +0300

    Вынес часть кода в ячейку

--- 1l_2S/1l_2S_Mihailov_Mihail/Anime Controller/Anime Deck/CustomCollectionViewCell.swift
+++ 1l_2S/1l_2S_Mihailov_Mihail/Anime Controller/Anime Deck/CustomCollectionViewCell.swift
@@ -5,5 +5,14 @@ import UIKit
 class CustomCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imageAnime: UIImageView!
     
+    let mainProfile = MainProfile.instance
+    let friendProfile = FriendProfile.instance
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageAnime.translatesAutoresizingMaskIntoConstraints = false
+        imageAnime.isUserInteractionEnabled = true
+        
+    }
+
 }
